ed9080778ea60b8bbf4a2acdf18554921a9cc41d
Long Duy <longduy124.work@gmail.com>
2026-06-13 23:24:20 +0700

    init project, create html fake datasets and implement JSoup

---
+++ mini_search_engine/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.jsoup:jsoup:1.17.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
