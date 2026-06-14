b1707e6efe1d093a2aeaa8aa17d998c4575b91b7
Long Duy <longduy124.work@gmail.com>
2026-06-14 14:50:44 +0700

    filter .DS-Store file

--- mini_search_engine/src/main/java/dataset_loader/DatasetLoader.java
+++ mini_search_engine/src/main/java/dataset_loader/DatasetLoader.java
@@ -14,6 +14,9 @@ public class DatasetLoader {
         HtmlParser parser = new HtmlParser();
         List<Page> pages = new ArrayList<>();
         for(File htmlFile : folder.listFiles()) {
+            if(htmlFile.getName().equals(".DS_Store")) {
+                continue;
+            }
             pages.add(parser.parse(htmlFile));
         }
         return pages;
