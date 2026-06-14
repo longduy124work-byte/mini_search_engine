3712d7d812a5741107e97fdc390facfe62de6bb5
Long Duy <longduy124.work@gmail.com>
2026-06-14 21:44:46 +0700

    test indexer in main method

--- mini_search_engine/src/main/java/org/example/Main.java
+++ mini_search_engine/src/main/java/org/example/Main.java
@@ -1,16 +1,33 @@
 package org.example;
-
 import dataset_loader.DatasetLoader;
+import indexer.Indexer;
 import model.Page;
+import posting.Posting;
+
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
-    static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         DatasetLoader loader = new DatasetLoader();
         List<Page> pages = loader.load("dataset");
-        for(Page page : pages) {
-            System.out.println(page);
+        Indexer indexer = new Indexer();
+        indexer.buildIndex(pages);
+
+        Map<String, List<Posting>> mapInvertedIndex = indexer.getMapInvertedIndex();
+
+        String[] testWords = {"python", "programming", "machine"};
+        for(String word : testWords) {
+            System.out.println("===" + word + "===");
+            List<Posting> postings = mapInvertedIndex.get(word);
+            if(postings == null) {
+                System.out.println("Không tìm thấy");
+                continue;
+            }
+            for(Posting p: postings) {
+                System.out.println(p);
+            }
         }
     }
 }
