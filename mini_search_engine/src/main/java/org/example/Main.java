1beaee7cfeac5b3f10f7450f846b1024c1232167
Long Duy <longduy124.work@gmail.com>
2026-06-20 23:36:09 +0700

    test page rank implementation

--- mini_search_engine/src/main/java/org/example/Main.java
+++ mini_search_engine/src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 import dataset_loader.DatasetLoader;
-import indexer.Indexer;
+import link_graph_builder.LinkGraphBuilder;
 import model.Page;
-import posting.Posting;
+import page_rank_calculator.PageRankCalculator;
 
 import java.io.IOException;
 import java.util.List;
@@ -12,22 +12,40 @@ public class Main {
     public static void main(String[] args) throws IOException {
         DatasetLoader loader = new DatasetLoader();
         List<Page> pages = loader.load("dataset");
-        Indexer indexer = new Indexer();
-        indexer.buildIndex(pages);
+//        TextProcessor processor = new TextProcessor();
+        LinkGraphBuilder linkGraphBuilder = new LinkGraphBuilder();
+        Map<String, List<String>> graph = linkGraphBuilder.buildGraph(pages);
+        Map<String, List<String>> reversedGraph = linkGraphBuilder.buildReverseGraph(graph);
+//        linkGraphBuilder.printGraph(graph, "GRAPH (outbound)");
+//        linkGraphBuilder.printGraph(reversedGraph, "REVERSED GRAPH (inbound)");
 
-        Map<String, List<Posting>> mapInvertedIndex = indexer.getMapInvertedIndex();
+        PageRankCalculator pageRankCalculator = new PageRankCalculator();
+        Map<String, Double> pageRanks = pageRankCalculator.calculate(graph, reversedGraph);
+//        pageRanks.entrySet().stream()
+//                .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+//                .forEach(e -> System.out.printf("%-35s → %.6f%n", e.getKey(), e.getValue()));
 
-        String[] testWords = {"python", "programming", "machine"};
-        for(String word : testWords) {
-            System.out.println("===" + word + "===");
-            List<Posting> postings = mapInvertedIndex.get(word);
-            if(postings == null) {
-                System.out.println("Không tìm thấy");
-                continue;
-            }
-            for(Posting p: postings) {
-                System.out.println(p);
-            }
-        }
+//        for (Page page : pages) {
+//           List<String> tokens = processor.process(page.getContent());
+//            System.out.println(tokens);
+//        }
+//
+//        Indexer indexer = new Indexer();
+//        indexer.buildIndex(pages);
+//
+//        Map<String, List<Posting>> mapInvertedIndex = indexer.getMapInvertedIndex();
+//
+//        String[] testWords = {"python", "programming", "machine"};
+//        for(String word : testWords) {
+//            System.out.println("===" + word + "===");
+//            List<Posting> postings = mapInvertedIndex.get(word);
+//            if(postings == null) {
+//                System.out.println("Không tìm thấy");
+//                continue;
+//            }
+//            for(Posting p: postings) {
+//                System.out.println(p);
+//            }
+//        }
     }
 }
